703a25b22f429faea77e8a36226045d0cab09407
romanahmed <roman.a.ahmed@gmail.com>
2018-07-06 14:46:08 +1000

    included unweighted forecasting

--- scripts/tsModelling.R
+++ scripts/tsModelling.R
@@ -39,18 +39,18 @@ combPlotData$hrvTSc <- round(exp(hrvTSc)-1)
 # Plot raw vs cleaned data
 combPlotData %>%
   ggplot(aes(visit_date, airReservedVis)) +
-  geom_line() +
-  geom_smooth(aes(visit_date, airReservedVis),
-              method = "loess", color = "darkgreen", span = 0.075) +
-  geom_line(aes(y=arvTSc), color = "green") +
-  geom_smooth(aes(visit_date, arvTSc),
-              method = "loess", color = "green", span = 0.075) +
-  geom_line(aes(y=hpgReservedVis)) +
-  geom_smooth(aes(visit_date, hpgReservedVis),
-              method = "loess", color = "maroon", span = 0.075)+
-  geom_line(aes(y=hrvTSc), color = "tomato") +
-  geom_smooth(aes(visit_date, hrvTSc),
-              method = "loess", color = "tomato", span = 0.075) +
+  geom_line(color = airResCol, alpha = 0.3, size = 2) +
+  # geom_smooth(aes(visit_date, airReservedVis),
+  #             method = "loess", color = "darkgreen", span = 0.075) +
+  geom_line(aes(y=arvTSc), color = airResCol, size = 0.7) +
+  # geom_smooth(aes(visit_date, arvTSc),
+  #             method = "loess", color = "green", span = 0.075) +
+  geom_line(aes(y=hpgReservedVis), color = hpgResCol, alpha = 0.3, size = 2) +
+  # geom_smooth(aes(visit_date, hpgReservedVis),
+  #             method = "loess", color = "maroon", span = 0.075)+
+  geom_line(aes(y=hrvTSc), color = hpgResCol, size = 0.7) +
+  # geom_smooth(aes(visit_date, hrvTSc),
+  #             method = "loess", color = "tomato", span = 0.075) +
   labs(y = "Total visitors all AIR stores", x = "Date") +
   theme_bw() +
   labs(title = 
@@ -65,6 +65,9 @@ xRegsFit <- log1p(combPlotData[-which(is.na(combPlotData$airVisitor) == TRUE),
 xRegsFore <- log1p(combPlotData[which(is.na(combPlotData$airVisitor) == TRUE),
                                 c("arvTSc", "hrvTSc")])
 
+arimaFitNoReg <- auto.arima(tvTS)
+aaForeNoReg <- forecast(arimaFitNoReg, h = 39)
+                   
 arimaFitYK <- auto.arima(tvTS, xreg = xRegsFit)
 aaFore <- forecast(arimaFitYK, xreg = xRegsFore)
 
@@ -74,37 +77,52 @@ combPlotData$Fitted[1:(min(which(is.na(combPlotData$airVisitor)))-1)] <- exp(c(a
 combPlotData$Forecast[which(is.na(combPlotData$airVisitor) == TRUE)] <- exp(c(aaFore$mean))-1
 combPlotData$ForecastLo[which(is.na(combPlotData$airVisitor) == TRUE)] <- exp(c(aaFore$lower[,1]))-1
 combPlotData$ForecastHi[which(is.na(combPlotData$airVisitor) == TRUE)] <- exp(c(aaFore$upper[,1]))-1
+combPlotData$ForecastNoAdj[which(is.na(combPlotData$airVisitor) == TRUE)] <- exp(c(aaForeNoReg$mean))-1
 
 
 
 # Plot raw vs cleaned data
 combPlotData %>%
   ggplot(aes(visit_date, airVisitor)) +
-  geom_line() +
-  geom_smooth(aes(visit_date, airVisitor),
-              method = "loess", color = "blue", span = 0.075) +
-  geom_line(aes(y=Fitted), color = "firebrick") +
-  geom_line(aes(y=Forecast), size = 1.2, color = "firebrick") +
-  geom_line(aes(y=ForecastLo), size = 1.2, color = "lightblue") +
-  geom_line(aes(y=ForecastHi), size = 1.2, color = "lightblue") +
+  geom_line(size = 0.8, color = airVisCol) +
+  # geom_smooth(aes(visit_date, airVisitor),
+  #             method = "loess", color = "blue", span = 0.075) +
+  geom_line(aes(y=Fitted), color = wes_palette(colPal)[1]) +
+  geom_line(aes(y=Forecast), size = 1.2, color = wes_palette(colPal)[1]) +
+  geom_line(aes(y=ForecastLo), size = 1.2, color = wes_palette(colPal)[5]) +
+  geom_line(aes(y=ForecastHi), size = 1.2, color = wes_palette(colPal)[5]) +
   geom_line(aes(y=airReservedVis)) +
-  geom_smooth(aes(visit_date, airReservedVis),
-              method = "loess", color = "darkgreen", span = 0.075) +
-  geom_line(aes(y=arvTSc), color = "green") +
-  geom_smooth(aes(visit_date, arvTSc),
-              method = "loess", color = "green", span = 0.075) +
+  # geom_smooth(aes(visit_date, airReservedVis),
+              # method = "loess", color = "darkgreen", span = 0.075) +
+  geom_line(aes(y=arvTSc),  size = 0.8, color = airResCol) +
+  # geom_smooth(aes(visit_date, arvTSc),
+  #             method = "loess", color = "green", span = 0.075) +
   geom_line(aes(y=hpgReservedVis)) +
-  geom_smooth(aes(visit_date, hpgReservedVis),
-              method = "loess", color = "maroon", span = 0.075)+
-  geom_line(aes(y=hrvTSc), color = "tomato") +
-  geom_smooth(aes(visit_date, hrvTSc),
-              method = "loess", color = "tomato", span = 0.075) +
+  # geom_smooth(aes(visit_date, hpgReservedVis),
+  #             method = "loess", color = "maroon", span = 0.075)+
+  geom_line(aes(y=hrvTSc),  size = 0.8, color = hpgResCol) +
+  # geom_smooth(aes(visit_date, hrvTSc),
+  #             method = "loess", color = "tomato", span = 0.075) +
   labs(y = "Total visitors all AIR stores", x = "Date") +
   theme_bw() +
   labs(title = 
          "Total guests reserving (using AIR and HPG) and visiting AIR stores over time") -> ForerawVsCleanesTS
-ForerawVsCleanesTS + scale_y_continuous(trans = "log1p")
+ForerawVsCleanesTSLog <- ForerawVsCleanesTS + scale_y_continuous(trans = "log1p")
 
 
-pb2 <- plotly_build(ForerawVsCleanesTS)
+
+# Compare adjust vs no adustment
+combPlotData %>%
+  ggplot(aes(visit_date, airVisitor)) +
+  geom_line(size = 0.8, color = airVisCol) +
+  geom_line(aes(y=Forecast), size = 1.2, alpha = 0.5, color = wes_palette(colPal)[1]) +
+  geom_line(aes(y=ForecastNoAdj), size = 1.2,  alpha = 0.5, color = "orchid") +
+  labs(y = "Observed and forecasted visitors all AIR stores", x = "Date") +
+  theme_bw() +
+  labs(title = "Comparing adjusted vs unadjusted (purple) forecast")-> foreCompAdjvsNoAdj 
+
+foreCompAdjvsNoAdjLog <- foreCompAdjvsNoAdj + scale_y_continuous(trans = "log1p")
+
+
+# pb2 <- plotly_build(ForerawVsCleanesTS)
 # pb2
